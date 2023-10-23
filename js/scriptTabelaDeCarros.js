c94713cbe669e76a11119f8ac1c6525a6d05b162
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-10-23 10:22:14 -0300

    refactor

--- js/scriptTabelaDeCarros.js
+++ js/scriptTabelaDeCarros.js
@@ -1,24 +1,18 @@
-// menu hamburguer 
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menuToggle');
-    const menuToggle2 = document.getElementById('menuToggle2');
-    const navList = document.querySelector('.nav_list');
+// menu hamburguer
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menuToggle");
+  const menuToggle2 = document.getElementById("menuToggle2");
+  const navList = document.querySelector(".nav_list");
 
+  menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    menuToggle2.classList.toggle("active");
+    navList.classList.toggle("active");
+  });
 
-    menuToggle.addEventListener('click', function () {
-        menuToggle.classList.toggle('active');
-        menuToggle2.classList.toggle('active')
-        navList.classList.toggle('active');
-    });
-
-
-    menuToggle2.addEventListener('click', () => {
-        navList.classList.toggle('active')
-        menuToggle.classList.toggle('active');
-        menuToggle2.classList.toggle('active')
-
-
-    })
-
-
+  menuToggle2.addEventListener("click", () => {
+    navList.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+    menuToggle2.classList.toggle("active");
+  });
 });
